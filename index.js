13cd465313a880ca9a2b609055d5ee8a0ec5e1db
秋野 <harky@vip.qq.com>
2023-06-18 19:57:32 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function convertUrlsToString(urls, prefix) {
+    return urls.map(obj => obj.url.replace(new RegExp(`.*${prefix}/`), '')).join(',');
+}
+
+module.exports = {
+    convertUrlsToString
+}
