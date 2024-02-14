4be1f2ed013e14ce402b28680e7b773679c0d178
akinowild <kyle.jee@gmail.com>
2024-02-14 10:25:54 +0800

    fix: 修改名字

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { findValueAndIndex, calculateAverage } from './lib/arrow/index'
 import { convertUrlsToString, weather} from './lib/weather/index'
 import { getURLParameters } from './lib/url/index'
 
-const akino ={ findValueAndIndex, calculateAverage,convertUrlsToString, weather,getURLParameters }
-export default akino
+const wild ={ findValueAndIndex, calculateAverage,convertUrlsToString, weather,getURLParameters }
+export default wild
